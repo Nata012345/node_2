f22cd6febf82ebcda3ec7756a0187d00b8782be0
Natalia Sereda <seredanatasha@gmail.com>
2024-09-07 20:23:02 +0400

    task03 - make separent url for summing numbers

--- index.js
+++ index.js
@@ -2,25 +2,40 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const handlers = {
+    '/sum' : sum,
+};
+
 const server = http.createServer((req, res) => {
     parseBodyJson(req, (err, payload) => {
-        if (err) {
-            res.statusCode = 400;
-            res.setHeader('Content-Type', 'application/json');
-            res.end('Invalid JSON');
-            return;
-        }
-        const c = { c: payload.a + payload.b }
+        const handler = getHandler(req.url);
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(c));
+        handler(req, res, payload, (err, result) => {
+            if (err) {
+                res.statusCode = err.code;
+                res.setHeader('Content-Type', 'application/json');
+                res.end( JSON.stringify(err) );
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end( JSON.stringify(result) );
+        });
     });
 });
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
-})
-
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+function getHandler(url) {
+    return handlers[url] || notFound;
+}
+function sum(req, res, payload, cb) {
+    const result = { c: payload.a + payload.b };
+    cb(null, result);
+}
+function notFound(req, res, payload, cb) {
+    cb({ code: 404, message: 'Not found' });
+}
 function parseBodyJson(req, cb) {
     let body = [];
     req.on('data', function(chunk) {
