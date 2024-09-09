35fbb527aba0debcb29f2cd05219ce45dea51d79
Natalia Sereda <seredanatasha@gmail.com>
2024-09-09 16:44:46 +0400

    add update and createLog functions

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 const hostname = '127.0.0.1';
 const port = 3000;
 const filePath = './api/articles.json';
 const articles = [];
+const logPath = path.json(__dirname, 'log.txt');
 
 const handlers = {
     '/api/articles/readall' : readArticles,
     '/api/articles/read' : readArticlesById,
     '/api/articles/create' : addNewArticle,
-    // '/api/articles/update' : sum,
+    '/api/articles/update' : updateArticleById,
     '/api/articles/delete' : deleteArticleById,
     // '/api/comments/create' : sum,
     // '/api/comments/delete' : sum,
@@ -27,6 +29,9 @@ const server = http.createServer((req, res) => {
             res.statusCode = result.statusCode;
             res.setHeader('Content-Type', 'application/json');
             res.end( JSON.stringify(result.data) );
+
+            // записываем в файл log время + eventType + statusCode
+            createLog(req, res, body);
         });
     });
 });
@@ -38,7 +43,8 @@ function readArticles(req, res, body, cb){
     const data = fs.readFileSync(filePath, {encoding: 'utf8', flag: 'r'});
     const fbData = {
         data: data,
-        statusCode: 200
+        statusCode: 200,
+        eventType: req.url
     }
     cb(null, fbData);
 }
@@ -54,7 +60,24 @@ function addNewArticle(req, res, body, cb) {
     writeArticleToFile(newArticle);
     const fbData = {
         data: newArticle,
-        statusCode: 201
+        statusCode: 201,
+        eventType: req.url
+    }
+    cb(null, fbData);
+}
+function updateArticleById(req, res, body, cb) {
+    const id = findIdValue(req);
+    const article = findId(id, articles);
+    let newParams = body;
+    for(let key in newParams){
+        article[key] = newParams[key];
+    }
+    articles[id - 1] = article;
+    writeFile(articles);
+    const fbData = {
+        data: article,
+        statusCode: 200,
+        eventType: req.url
     }
     cb(null, fbData);
 }
@@ -65,39 +88,42 @@ function deleteArticleById(req, res, body, cb){
     if (article) {
         const articleIndex = id - 1;
         articles.splice(articleIndex, 1);
+        rewriteID();
         writeFile(articles);
         fbData = {
-            data: article,
-            statusCode: 200
+            data: articles,
+            statusCode: 200,
+            eventType: req.url
         }
     } else {
         fbData = {
             data: {'err': 'article not found'},
-            statusCode: 404
+            statusCode: 404,
+            eventType: req.url
         }
     }
     cb(null, fbData);
 }
 function readArticlesById(req, res, body, cb) {
     const id = findIdValue(req);
-    let fbData = null;
     const article = findId(id, articles);
+    let fbData = null;
     if (article) {
         fbData = {
             data: article,
-            statusCode: 200
+            statusCode: 200,
+            eventType: req.url
         }
     } else {
         fbData = {
             data: {'err': 'article not found'},
-            statusCode: 404
+            statusCode: 404,
+            eventType: req.url
         }
     }
     cb(null, fbData);
 }
 // -------------------- вспомогательные функции -------------------
-function findArticleById(){i
-}
 function findIdValue(req) {
     const current_url = new URL('http://localhost' + req.url);
     const search_params = current_url.searchParams;
@@ -111,11 +137,27 @@ function writeArticleToFile(article) {
 function findId(id, articles) {
     return articles.find(item => item.id === id);
 }
+function rewriteID() {
+    for (let i = 0; i < articles.length; i++) {
+        articles[i].id = i;
+    }
+}
 // -------------------- функции чтения записи -------------------
 function writeFile(data){
     fs.writeFile(filePath, JSON.stringify(data), {encoding : 'utf8', flag: 'w'}, (err) => {});
 }
 // -------------------- функции сервера -------------------
+function createLog(req, res, body) {
+    const c = newDate().toString();
+    const logRaw =
+        `
+        ${time}
+        ${req.url}
+        ${req.method}
+        ${body ? body : 'no body'}
+      ------------------------------\n`;
+    fs.appendFile(logPath, logRaw, 'utf8', (err)=>{});
+}
 function getHandler(url) {
     let urlSplit = url.split('?')[0];
     return handlers[urlSplit] || notFound;
